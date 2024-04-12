d2f8b64d6192c1f4bdc8e2356f2c4e5aa84f2738
macdonald34 <+>
2024-04-12 12:18:35 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -18,21 +18,47 @@ db.init_app(app)
 def index():
     return '<h1>Bakery GET API</h1>'
 
-@app.route('/bakeries')
+@app.route('/bakeries', methods=['GET'])
 def bakeries():
-    return ''
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+    response = make_response(jsonify(bakeries))
+    return response
 
-@app.route('/bakeries/<int:id>')
+@app.route('/bakeries/<int:id>', methods=['GET'])
 def bakery_by_id(id):
-    return ''
+    try:
+        bakery = Bakery.query.filter(Bakery.id==id).first()
+        if not bakery:
+            raise  ValueError(f"No bakery with id {id}")
+        else:    
+            bakery_dict = bakery.to_dict()    
+            response = make_response(jsonify(bakery_dict), 200)
+            return response
+    except Exception as e:
+        return jsonify({"error":"No bakery with that ID", "message": str(e)}), 404
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
-
+    try:
+        goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+        baked_goods = [good.to_dict() for good in goods]
+        response = make_response(jsonify(baked_goods),200)
+        return response
+    except  Exception as e:
+        return jsonify({'error': 'Failed to retrieve data','message':str(e)}),500
+    
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    try:
+        most_expensive_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+        if most_expensive_good:
+            baked_good_dict = most_expensive_good.to_dict()
+            response = make_response(jsonify(baked_good_dict), 200)
+            return response
+        else:
+            return jsonify({'error':'There are no baked goods on record.'}), 404        
+    except  Exception as e:
+        return jsonify({'error': 'Failed to retrieve data','message':str(e)}), 500
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
